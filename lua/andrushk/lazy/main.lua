fe4024e0c145bfad84e896e8613ccd7daea88eb9
Andrea Mangione <andrea@mangione.dev>
2024-02-12 12:33:20 +0100

    add commentary and scrolling for C-d and C-u

--- lua/andrushk/lazy/main.lua
+++ lua/andrushk/lazy/main.lua
@@ -1,6 +1,21 @@
 return {
 	{ 'nvim-lua/plenary.nvim', name = "plenary" },
 	{
+		'tpope/vim-commentary'
+	},
+	{
+		'declancm/cinnamon.nvim',
+		config = function()
+			require('cinnamon').setup { default_delay = 3 }
+			vim.keymap.set('n', 'gd', "<Cmd>lua Scroll('definition')<CR>")
+			-- Add scrilling on half page scroll to avoid people dizziness looking at my screen
+			vim.keymap.set({ 'n', 'x' }, '<C-u>',
+				"<Cmd>lua Scroll('<C-u>', 1, 1)<CR><Cmd>lua Scroll('zz', 0, 1)<CR>")
+			vim.keymap.set({ 'n', 'x' }, '<C-d>',
+				"<Cmd>lua Scroll('<C-d>', 1, 1)<CR><Cmd>lua Scroll('zz', 0, 1)<CR>")
+		end
+	},
+	{
 		"EdenEast/nightfox.nvim",
 		name = "nightfox",
 		config = function()
--- lua/andrushk/remap.lua
+++ lua/andrushk/remap.lua
@@ -11,6 +11,8 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Moving half page leaves the cursor in the center
+-- This is overridden by cinnamon, so it is here as backup
+-- if the plugin is disabled
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
