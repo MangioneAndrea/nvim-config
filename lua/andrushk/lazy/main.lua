8ee19b6f0f5fb95defb402178e65d9c8a0c490cc
Andrea Mangione <andrea@mangione.dev>
2025-03-02 22:12:52 +0100

    feat: upgrade cinnamon

--- lua/andrushk/lazy/main.lua
+++ lua/andrushk/lazy/main.lua
@@ -6,13 +6,18 @@ return {
 	{
 		'declancm/cinnamon.nvim',
 		config = function()
-			require('cinnamon').setup { default_delay = 3 }
-			vim.keymap.set('n', 'gd', "<Cmd>lua Scroll('definition')<CR>")
-			-- Add scrilling on half page scroll to avoid people dizziness looking at my screen
-			vim.keymap.set({ 'n', 'x' }, '<C-u>',
-				"<Cmd>lua Scroll('<C-u>', 1, 1)<CR><Cmd>lua Scroll('zz', 0, 1)<CR>")
-			vim.keymap.set({ 'n', 'x' }, '<C-d>',
-				"<Cmd>lua Scroll('<C-d>', 1, 1)<CR><Cmd>lua Scroll('zz', 0, 1)<CR>")
+			local cinnamon = require("cinnamon")
+			cinnamon.setup { options = { delay = 3 } }
+			vim.keymap.set('n', 'gd', function()
+				cinnamon.scroll('definition')
+			end)
+			-- Add scrilling on half page Scroll to avoid people dizziness looking at my screen
+			vim.keymap.set({ 'n', 'x' }, '<C-u>', function()
+				cinnamon.scroll('<C-u>zz')
+			end)
+			vim.keymap.set({ 'n', 'x' }, '<C-d>', function()
+				cinnamon.scroll('<C-d>zz')
+			end)
 		end
 	},
 	{
@@ -24,7 +29,16 @@ return {
 	},
 	{
 		'prettier/vim-prettier',
-		run = "npm i -g prettier"
+		run = "npm i -g prettier",
+		config = function()
+			vim.cmd([[
+				"avoid error info write to header of buffer
+				let g:prettier#exec_cmd_async = 1
+     		 	]])
+		end,
 	},
+	{
+		'sveltejs/prettier-plugin-svelte'
+	}
 
 }
